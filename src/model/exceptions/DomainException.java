03a6daee248c66bdb9612ef712a19d3ab57689fa
zignini <jacqueline.pruschinski@gmail.com>
2023-12-12 17:10:10 -0300

    feat: treats exceptions (better)

---
+++ src/model/exceptions/DomainException.java
@@ -0,0 +1,13 @@
+package model.exceptions;
+
+import java.io.Serial;
+
+public class DomainException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DomainException (String message) {
+        super(message);
+    }
+}
